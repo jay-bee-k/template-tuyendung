d96b0a188c3fc923c4838a96aad159049a92fdf7
Jaybee-k <79182784+JayBee-K@users.noreply.github.com>
2022-08-09 22:17:37 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,60 @@
 ;(function ($) {
-	'use strict';
-	let windowWidth = $(window).width();
+    'use strict';
+    let windowWidth = $(window).width();
 
-	let handleTouchMoveNavigation = function (ev) {
-		if (!$(ev.target).closest('#header-navigation').length) {
-			ev.preventDefault();
-		}
-	}
-	let handleHeaderMobile = () => {
-		if (windowWidth < 992) {
+    let handleTouchMoveNavigation = function (ev) {
+        if (!$(ev.target).closest('#header-navigation').length) {
+            ev.preventDefault();
+        }
+    }
+    let handleHeaderMobile = () => {
+        if (windowWidth < 992) {
 
-		}
-	}
+        }
+    }
 
+    let handleDropdownBanner = function () {
+        if ($('.initDropdown').length) {
+            $('.initDropdown .initDropdown-btn').click(function () {
+                let btn = $(this),
+                    elmParent = btn.parent();
+                if (elmParent.hasClass('is-show')) {
+                    elmParent.removeClass('is-show');
+                } else {
+                    if ($('.initDropdown').hasClass('is-show')) {
+                        $('.initDropdown').removeClass('is-show')
+                    }
+                    elmParent.addClass('is-show');
+                }
+            });
 
-	$(document).ready(function () {
-		handleHeaderMobile();
-	});
+            $('.initDropdown .initDropdown_value').click(function () {
+                let btn = $(this),
+                    value = btn.html(),
+                    elmParent = btn.parents('.initDropdown'),
+                    step = parseInt(elmParent.data('step')) + 1;
+
+                elmParent.removeClass('is-show');
+                elmParent.children('.initDropdown-btn').html(value);
+
+                elmParent.find('.initDropdown_value').removeClass('is-active');
+                btn.addClass('is-active');
+
+                $('.initDropdown[data-step=' + step + ']').addClass('is-show')
+            });
+
+            $(document).on('mouseup', function (event) {
+                let targetElm = $(".initDropdown.is-show");
+                targetElm.is(event.target) || 0 !== targetElm.has(event.target).length || (
+                    targetElm.removeClass('is-show'))
+            });
+        }
+    };
+
+    $(document).ready(function () {
+        handleHeaderMobile();
+        handleDropdownBanner();
+    });
 
 })
 (jQuery);
